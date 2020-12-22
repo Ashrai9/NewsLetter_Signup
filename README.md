2500a7b0f24c8d7b4f9cd74964c36e5e0a2a20d5
Ashwini Rai <sashwinirai@gmail.com>
2020-12-22 17:53:22 +0530

    Update the Authorisation details

---
+++ README.md
@@ -0,0 +1,7 @@
+Newsletter Subscription:
+Subscription to the newsletter through signup page can be done, consequently provides success or failure message.
+
+API Authorisation:
+a>Open .env_sample file and rename "Type your API key here" with your API key
+b>Rename .env_sample to .env
+
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
 
+require('dotenv').config()
+
 const app = express()
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -31,7 +33,7 @@ app.post("/", function (req, res) {
         url: "https://us7.api.mailchimp.com/3.0/lists/fbb229e37b",
         method: "POST",
         headers: {
-            "Authorization": "ash1 8d06e6c2786afd6777b03e0bda5b0f6d-us7"
+            "Authorization": process.env.API_KEY
         },
         //body: jsonData
 
